7e9b679e1b662bc29a25c3fcf16b7dd92688e758
nithyaravi8198-tech <nithyaravi8198@gmail.com>
2026-03-03 21:32:31 +0530

    Updated message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI/CD pipeline!");
+        System.out.println("Build triggered from Github!");
     }
 }
